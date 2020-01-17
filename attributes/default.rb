74de06d1a98f7f71628225b1b77b53dd837a366d
Richard Baker <richard.baker@digital.cabinet-office.gov.uk>
2020-01-17 14:29:54 +0000

    Support installer version attribute
    Allows the installer script version to be pinned.
    Defaults to 'latest'.

--- attributes/default.rb
+++ attributes/default.rb
@@ -2,3 +2,4 @@ default['cwlogs']['region'] = 'us-east-1'
 default['cwlogs']['state_file_dir'] = '/var/awslogs/state'
 default['cwlogs']['state_file_name'] = 'agent-state'
 default['cwlogs']['attempt_upgrade'] = true
+default['cwlogs']['installer_version'] = 'latest'
